1b394c2d884d8667a29e116793ebb1f9aba43b00
rpilay <95061471+rpilay@users.noreply.github.com>
2022-10-02 11:11:54 -0500

    Create segundo_ejercicio.js

---
+++ Primer Proyecto Node/segundo_ejercicio.js
@@ -0,0 +1,23 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+let nombre ="Ronald Fabricio Pilay Morán";
+let edad = 42;
+let eresDesarrollador = true;
+let fechaNacimiento = new Date("December 13, 1979")
+const libroFavorito ={
+    titulo: "Fuego y Sangre",
+    autor: "GEORGE R.R. MARTIN ",
+    fecha: new Date("November 20,2018"),
+    url:"https://www.casadellibro.com/libro-fuego-y-sangre-cancion-de-hielo-y-fuego/9788401022166/6807253"    
+}
+console.log(libroFavorito)
